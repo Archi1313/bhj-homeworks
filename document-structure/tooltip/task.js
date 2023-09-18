992f776e0d0b3cc4ccebbb4902bf49b2d5257cfc
Archi1313 <131608631+Archi1313@users.noreply.github.com>
2023-09-19 00:39:20 +0300

    Add files via upload

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,14 @@
+const tooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+for (let el of tooltip) {
+		let elem = document.createElement('div');
+		elem.classList="tooltip";
+		elem.textContent=el.title;
+		el.appendChild(elem);
+	}
+for (let el of tooltip) {
+	el.addEventListener('click', function(event){
+		event.preventDefault();
+		
+		el.querySelector('div').classList.toggle('tooltip_active');
+	})
+}
