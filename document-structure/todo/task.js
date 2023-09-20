40890b72a44ce02b3370a2ca4a650a2732a0e2a9
Archi1313 <131608631+Archi1313@users.noreply.github.com>
2023-09-20 18:26:27 +0300

    Add files via upload

---
+++ document-structure/todo/task.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('input');
+const taskList = document.querySelector('.tasks__list');
+let remove;
+
+input.addEventListener('keyup', function (event) {
+	if ((event.code==='Enter')&(input.value!='')) {
+		taskList.innerHTML+=`<div class="task">
+  <div class="task__title">`
+    +input.value+
+  `</div>
+  <a href="#" class="task__remove">&times;</a>
+</div>`
+	input.value='';
+	}
+	 remove=taskList.querySelectorAll('.task__remove');
+})
+
+taskList.addEventListener('click', function (event){
+	if (Array.from(remove).includes(event.target)){
+		event.target.closest('.task').remove();
+	}
+})
